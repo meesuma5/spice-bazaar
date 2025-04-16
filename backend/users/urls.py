1405d5825de5b9a51f90a972d702fe7e2baf8f1f
anya k <116333070+haniakashif@users.noreply.github.com>
2025-04-16 23:07:12 +0500

    create bookmarks, delete bookmarks
    user can create bookmarks on recipes and delete bookmarks from recipes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserUpdateView
+from .views import RegisterView, LoginView, UserUpdateView, BookmarkCreateView, BookmarkDeleteView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('update/', UserUpdateView.as_view(), name='update-user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('bookmark/', BookmarkCreateView.as_view(), name='create-bookmark'),
+    path('bookmark/<uuid:recipe_id>/delete/', BookmarkDeleteView.as_view(), name='delete-bookmark'),
 ]
