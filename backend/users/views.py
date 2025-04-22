0529e7603c4e80a927d52a3d569bfacde919dcba
Raahim Hashmi <rh08461@st.habib.edu.pk>
2025-04-22 11:49:26 +0500

    Logout added

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -46,7 +47,18 @@ class UserUpdateView(generics.UpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class BookmarkCreateView(generics.CreateAPIView):
     serializer_class = BookmarkCreateSerializer
